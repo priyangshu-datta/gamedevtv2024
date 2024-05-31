810af5524cd908a820507c76fb25c4a3c1394ecf
Priyangshu Datta <dattapapun2468@gmail.com>
2024-05-31 15:33:27 +0000

    updated floor to support breakable roof

--- sprites/Floor.py
+++ sprites/Floor.py
@@ -1,10 +1,10 @@
 from settings import *
 
+
 class Floor(pygame.sprite.Sprite):
     def __init__(self, pos: pygame.Vector2, surf: pygame.Surface, groups) -> None:
         super().__init__(groups)
-        
+
         self.image = surf
         self.rect = self.image.get_frect(topleft=pos)
-        
-        
+        self.hp = 5
